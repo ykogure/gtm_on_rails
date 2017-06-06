9138f0674bdf6ba317e53e6a294c9c243dbdf426
ykogure <renkin1008@gmail.com>
2017-06-06 11:57:12 +0900

    minor_update_20170606 (#3)

--- lib/gtm_on_rails/models/data_layer.rb
+++ lib/gtm_on_rails/models/data_layer.rb
@@ -22,7 +22,7 @@ module GtmOnRails
       end
     end
 
-    def print_on_js
+    def to_js
       js_codes = []
 
       js_codes << "var dataLayer = dataLayer || [];"
@@ -30,13 +30,18 @@ module GtmOnRails
         # dataLayer size limit exception
         size = data_layer_object.to_json.bytesize
         if size > GtmOnRails.config.data_layer_limit_byte_size
-          raise ArgumentError.new("DataLayer bytesize is over limit #{GtmOnRails.config.data_layer_limit_byte_size} bytes. Size is #{size} bytes.")
+          logger.warn("DataLayer bytesize is over limit #{GtmOnRails.config.data_layer_limit_byte_size} bytes. Size is #{size} bytes.")
+          next
         end
 
         js_codes << data_layer_object.to_js
       end
 
-      return content_tag(:script, js_codes.join.html_safe)
+      return js_codes.join.html_safe
+    end
+
+    def print_on_html
+      content_tag(:script, self.to_js)
     end
   end
 end
