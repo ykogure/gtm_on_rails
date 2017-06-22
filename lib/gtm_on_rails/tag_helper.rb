50a69febed1e577d994247f950521f815fcb88a8
ykogure <renkin1008@gmail.com>
2017-06-22 12:03:48 +0900

    Little Fix (#4)

--- lib/gtm_on_rails/tag_helper.rb
+++ lib/gtm_on_rails/tag_helper.rb
@@ -2,13 +2,13 @@ module GtmOnRails
   module TagHelper
     def render_gtm_on_rails_tag_in_head
       tags = []
+      tags << render(partial: 'gtm_on_rails/layouts/data_layer_tag')
       tags << render(partial: 'gtm_on_rails/layouts/google_tag_manager_tag_in_head')
       tags.join.html_safe
     end
 
     def render_gtm_on_rails_tag_in_body
       tags = []
-      tags << render(partial: 'gtm_on_rails/layouts/data_layer_tag')
       tags << render(partial: 'gtm_on_rails/layouts/google_tag_manager_tag_in_body')
       tags.join.html_safe
     end
--- lib/gtm_on_rails/version.rb
+++ lib/gtm_on_rails/version.rb
@@ -1,3 +1,3 @@
 module GtmOnRails
-  VERSION = '0.1.3'
+  VERSION = '0.1.4'
 end
